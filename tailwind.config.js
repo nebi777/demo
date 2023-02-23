90a631c888c7f61d8ef17bf99d3a67e47cbb61c1
Nebi <nabi-allahverdiyev@mail.ru>
2023-02-23 20:18:55 +0400

    test

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'mor': '#6355e4',
+      },
+    },
+  },
+  plugins: [],
+}
